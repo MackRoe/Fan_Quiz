aa7f6d9e09702597f56356112c8c0b614712ee9d
MackRoe <elaine.music@students.makeschool.com>
2019-11-15 09:40:41 -0800

    MVP framework complete

--- app.py
+++ app.py
@@ -8,22 +8,27 @@ import random
 
 
 def get_question():
-    ''' read question from file to variable '''
+    ''' read question from file to variable
+        return a string '''
 #     """ STRETCH - return a random question """
 #     new_question = random_question()
-    pass
+    return "GOT"
 
 
 def number_the_question():
+    question_number = "#"
     """ assign the sequential numbering of a question """
     return question_number
-    pass
 
 
 def test_answer():
     """ determine if correct answer to question was givin """
     pass
 
+
+def question_answers():
+    list_answers = ['a', 'b', 'c']
+    return list_answers
 
 ''' -- STRETCH --
 # def automate_questions():
@@ -34,8 +39,8 @@ def test_answer():
 #     pass '''
 
 def question1():
-    print(get_question())
-    print(answers)
+    print(str(number_the_question()) + get_question())
+    print(question_answers())
     response = input("Enter one letter for your answer [A, B, or C]:")
     print('You answered: ' + str(response))
 
@@ -51,5 +56,13 @@ def question3():
 def question4():
     pass
 
+
 def question5():
     pass
+
+
+def start_quiz():
+    question1()
+
+
+start_quiz()
