4f8f02bac0d63bd713f528ec0e64ffc567253c4e
MackRoe <elaine.music@students.makeschool.com>
2019-11-15 10:42:21 -0800

    excess functions removed. question1, with answers and answer input, displays

--- app.py
+++ app.py
@@ -1,46 +1,13 @@
 # fan quiz to run in Terminal
-import random
-
-
-# def random_question():
-#     """ STRETCH - get a random question from list of questions """
-#     pass
-
-
-def get_question():
-    ''' read question from file to variable
-        return a string '''
-#     """ STRETCH - return a random question """
-#     new_question = random_question()
-    return "GOT"
-
-
-def number_the_question():
-    question_number = "#"
-    """ assign the sequential numbering of a question """
-    return question_number
-
-
-def test_answer():
-    """ determine if correct answer to question was givin """
-    pass
-
-
-def question_answers():
-    list_answers = ['a', 'b', 'c']
-    return list_answers
-
-''' -- STRETCH --
-# def automate_questions():
-#     pass
-
-# --Concatonate the function call from variables --
-# def call_question():
-#     pass '''
 
 def question1():
-    print(str(number_the_question()) + get_question())
-    print(question_answers())
+    # print(str(number_the_question()) + get_question())
+    question1_text = "1. Name the park where most of the vampires hang out."
+    question1_answers = ['a) Fair Oaks Park', 'b) Weatherly Park', 'c) Mission City Memorial Park']
+    print(question1_text)
+    for answer in question1_answers:
+        print(answer)
+    # print(question_answers(question_number))
     response = input("Enter one letter for your answer [A, B, or C]:")
     print('You answered: ' + str(response))
 
