70bdab8b42a282924e1f80bdbd6b19983aab6eeb
MackRoe <elaine.music@students.makeschool.com>
2019-11-15 10:56:23 -0800

    tells user if question is correct

--- app.py
+++ app.py
@@ -4,12 +4,25 @@ def question1():
     # print(str(number_the_question()) + get_question())
     question1_text = "1. Name the park where most of the vampires hang out."
     question1_answers = ['a) Fair Oaks Park', 'b) Weatherly Park', 'c) Mission City Memorial Park']
+    question1_trivia = "In 'The Pack', the pack minus Xander slept there after they ate Principal Flutie. According to the book 'The Watcher's Guide Volume 1', the gates to Weatherly Park are closed at 10 p.m."
     print(question1_text)
     for answer in question1_answers:
         print(answer)
     # print(question_answers(question_number))
     response = input("Enter one letter for your answer [A, B, or C]:")
     print('You answered: ' + str(response))
+    if response.upper() == 'B':
+        print("Your answer is CORRECT")
+        print(question1_trivia)
+        correct = 1
+    elif response.upper() == 'A' or upper(response) == 'C':
+        print("The correct answer was B (Weatherly Park)")
+        print(question1_trivia)
+        correct = 0
+    else:
+        print("Try Again!")
+        response = input("Enter one letter for your answer [A, B, or C]: ")
+    return correct
 
 
 def question2():
