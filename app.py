ed7a9125b9f875a6d1d3171bdc1067a550a39d67
MackRoe <elaine.music@students.makeschool.com>
2019-11-15 09:31:29 -0800

    first commit, starter code

---
+++ app.py
@@ -0,0 +1,55 @@
+# fan quiz to run in Terminal
+import random
+
+
+# def random_question():
+#     """ STRETCH - get a random question from list of questions """
+#     pass
+
+
+def get_question():
+    ''' read question from file to variable '''
+#     """ STRETCH - return a random question """
+#     new_question = random_question()
+    pass
+
+
+def number_the_question():
+    """ assign the sequential numbering of a question """
+    return question_number
+    pass
+
+
+def test_answer():
+    """ determine if correct answer to question was givin """
+    pass
+
+
+''' -- STRETCH --
+# def automate_questions():
+#     pass
+
+# --Concatonate the function call from variables --
+# def call_question():
+#     pass '''
+
+def question1():
+    print(get_question())
+    print(answers)
+    response = input("Enter one letter for your answer [A, B, or C]:")
+    print('You answered: ' + str(response))
+
+
+def question2():
+    pass
+
+
+def question3():
+    pass
+
+
+def question4():
+    pass
+
+def question5():
+    pass
